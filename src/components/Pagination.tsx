a6613d434136656aeb14834bfd36a51519018201
DmitryLasuta <lasutadima01@gmail.com>
2023-12-19 07:38:49 +0300

    style: added flex-wrap

--- src/components/Pagination.tsx
+++ src/components/Pagination.tsx
@@ -1,3 +1,5 @@
+import React from 'react'
+
 interface PaginationProps {
   currentPage: number
   totalPages: number
@@ -8,12 +10,14 @@ export const Pagination = ({ currentPage, totalPages, onPageChange }: Pagination
   const pageNumbers = Array.from({ length: totalPages }, (_, i) => i + 1)
 
   return (
-    <div className="flex justify-center mt-4">
+    <div className="flex justify-center mt-4 flex-wrap gap-1">
       {pageNumbers.map(pageNumber => (
         <button
           key={pageNumber}
           onClick={() => onPageChange(pageNumber)}
-          className={`mx-2 px-4 py-2 ${pageNumber === currentPage ? 'bg-blue-500 text-white' : 'bg-gray-200'}`}
+          className={`px-2 py-1 text-sm ${
+            pageNumber === currentPage ? 'bg-blue-500 text-white' : 'bg-gray-200'
+          } md:mx-2 md:px-4 md:py-2 md:text-base`}
           type="button"
         >
           {pageNumber}
